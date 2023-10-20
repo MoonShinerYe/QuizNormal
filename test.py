8cad8e865c6ed888a6fce9ed29bf0816b69a328e
MoonShinerYe <maradonaegor@mail.ru>
2023-10-20 16:52:56 +0700

    hhhhghg

--- test.py
+++ test.py
@@ -67,6 +67,28 @@ class TestSurveyApplication(unittest.TestCase):
         self.assertTrue(self.app.question_label.pack.called)
         self.assertTrue(self.app.question_entry.pack.called)
         self.assertTrue(self.app.answer_label.pack.called)
+    
+    def setUp(self):
+        self.app = SurveyApplication()
+        self.app.open_file_flag = False
+
+    def test_show_result_correct(self):
+        self.app.open_file_flag = False
+        self.app.questions = [{'question': 'Question 1', 'answer': 'Answer 1'}]
+        self.app.question_index = 0
+        self.app.show_result(True)
+        result_label = self.app.winfo_children()[-1]  # Get the last label widget added to the application
+        expected_text = "1. Вопрос: Question 1 Ответ: Answer 1. Верно!"
+        self.assertEqual(result_label["text"], expected_text)
+
+    def test_show_result_incorrect(self):
+        self.app.open_file_flag = True
+        self.app.questions_txt = [['Question 1', 'Answer 1']]
+        self.app.question_num = 1
+        self.app.show_result(False)
+        result_label = self.app.winfo_children()[-1]  # Get the last label widget added to the application
+        expected_text = "1. Вопрос: Question 1 Ответ: Answer 1. Неверно!"
+        self.assertEqual(result_label["text"], expected_text)
 
 if __name__ == '__main__':
     unittest.main()
