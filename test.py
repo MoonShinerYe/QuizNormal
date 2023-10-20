8d511132c1de7b33303220d1649c6639007ec78f
MoonShinerYe <maradonaegor@mail.ru>
2023-10-20 16:48:08 +0700

    hhhhgh

--- test.py
+++ test.py
@@ -1,86 +1,72 @@
+
 import unittest
-from tkinter import Tk
-from unittest.mock import patch
-from io import StringIO
-from main import SurveyApplication
+from tkinter import filedialog
+from unittest.mock import patch, MagicMock
+from survey_application import SurveyApplication
 
-class SurveyApplicationTests(unittest.TestCase):
-
-    def setUp(self):
-        self.app = SurveyApplication()
-        self.app.title("QuizRunner")
-        self.app.geometry("500x500")
-
-    def test_init(self):
-        self.assertEqual(self.app.title(), "QuizRunner")
-        self.assertEqual(self.app.geometry(), "500x500")
-        self.assertFalse(self.app.resizable())
+class TestSurveyApplication(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = SurveyApplication()
+        cls.app.open_file_flag = 1
 
     def test_open_file(self):
-        with patch('tkinter.filedialog.askopenfilename', return_value='sample.txt'):
+        with patch('tkinter.filedialog.askopenfilename') as mock_askopenfilename:
+            mock_askopenfilename.return_value = "sample.txt"
             self.app.open_file()
-            self.assertEqual(self.app.filepath, 'sample.txt')
-            self.assertEqual(len(self.app.questions_txt), 2)
+            self.assertEqual(self.app.filepath, "sample.txt")
+
+    def test_loadbackground(self):
+        self.app.loadbackground()
+        self.assertIsNotNone(self.app.background_img)
 
     def test_show_title(self):
-        self.app.title_entry.insert(0, "Sample Quiz")
+        self.app.title_entry = MagicMock()
+        self.app.submit_title_btn = MagicMock()
+        self.app.title_img_label = MagicMock()
+        self.app.txtlbl = MagicMock()
+        self.app.create_widgets = MagicMock()
+
         self.app.show_title()
-        self.assertEqual(self.app.title_label['text'], "Sample Quiz")
-        self.assertFalse(self.app.title_entry.winfo_ismapped())
-        self.assertFalse(self.app.submit_title_btn.winfo_ismapped())
 
-    def test_add_question(self):
-        self.app.question_entry.insert(0, "What is the capital of France?")
-        self.app.answer_entry.insert(0, "Paris")
-        self.app.add_question()
-        self.assertEqual(len(self.app.questions), 1)
-        question = self.app.questions[0]
-        self.assertEqual(question['question'], "What is the capital of France?")
-        self.assertEqual(question['answer'], "Paris")
-        self.assertEqual(self.app.question_entry.get(), "")
-        self.assertEqual(self.app.answer_entry.get(), "")
+        self.assertEqual(self.app.title_label["text"], self.app.title_entry.get())
+        self.assertTrue(self.app.title_entry.pack_forget.called)
+        self.assertTrue(self.app.submit_title_btn.pack_forget.called)
+        self.assertTrue(self.app.title_img_label.pack_forget.called)
+        self.assertTrue(self.app.txtlbl.pack_forget.called)
+        self.assertTrue(self.app.create_widgets.called)
 
-    def test_line_switch(self):
-        question = "This is a long question that needs to be wrapped"
-        self.app.question_label.configure(text=question)
-        width_question = self.app.question_label.winfo_width()
-        self.app.line_switch(question)
-        new_width_question = self.app.question_label.winfo_width()
-        self.assertEqual(new_width_question, width_question)
+    def test_title_quiz(self):
+        self.app.title_label = MagicMock()
+        self.app.title_entry = MagicMock()
+        self.app.submit_title_btn = MagicMock()
+        self.app.title_picture_add = MagicMock()
 
-    def test_submit_answer(self):
-        self.app.open_file_flag = 0
-        self.app.questions = [{'question': 'Q1', 'answer': 'A1'}, {'question': 'Q2', 'answer': 'A2'}]
-        self.app.question_index = 0
-        self.app.answer_entry.insert(0, "A1")
-        self.app.submit_answer()
-        self.assertEqual(self.app.result_counter, 1)
-        self.assertEqual(self.app.result_label_str, "1. Вопрос: Q1 Ответ: A1. Верно!")
+        self.app.title_quiz()
 
-    def test_submit_quiz(self):
-        self.app.open_file_flag = 0
-        self.app.questions = [{'question': 'Q1', 'answer': 'A1'}, {'question': 'Q2', 'answer': 'A2'}]
-        self.app.submit_quiz_button.invoke()
-        self.assertFalse(self.app.question_entry.winfo_ismapped())
-        self.assertFalse(self.app.question_label.winfo_ismapped())
-        self.assertFalse(self.app.add_question_button.winfo_ismapped())
-        self.assertEqual(self.app.title_label['text'], "Опрос")
-        self.assertTrue(self.app.submit_button.winfo_ismapped())
+        self.assertTrue(self.app.title_label.pack.called)
+        self.assertTrue(self.app.title_entry.pack.called)
+        self.assertTrue(self.app.submit_title_btn.pack.called)
+        self.assertTrue(self.app.title_picture_add.called)
 
-    def test_show_result(self):
-        self.app.open_file_flag = 0
-        self.app.questions = [{'question': 'Q1', 'answer': 'A1'}, {'question': 'Q2', 'answer': 'A2'}]
-        self.app.question_index = 0
-        with patch('sys.stdout', new=StringIO()) as mock_stdout:
-            self.app.show_result(True)
-            output = mock_stdout.getvalue().strip()
-            expected_output = "1. Вопрос: Q1 Ответ: A1. Верно!"
-            self.assertEqual(output, expected_output)
+    def test_title_picture_add(self):
+        self.app.title_img = MagicMock()
+        self.app.title_img_label = MagicMock()
 
-            self.app.show_result(False)
-            output = mock_stdout.getvalue().strip()
-            expected_output += "\n2. Вопрос: Q1 Ответ: A1. Неверно!"
-            self.assertEqual(output, expected_output)
+        self.app.title_picture_add()
+
+        self.assertTrue(self.app.title_img_label.pack.called)
+
+    def test_create_widgets(self):
+        self.app.question_label = MagicMock()
+        self.app.question_entry = MagicMock()
+        self.app.answer_label = MagicMock()
+
+        self.app.create_widgets()
+
+        self.assertTrue(self.app.question_label.pack.called)
+        self.assertTrue(self.app.question_entry.pack.called)
+        self.assertTrue(self.app.answer_label.pack.called)
 
 if __name__ == '__main__':
     unittest.main()
