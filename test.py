bc2c6c9d993b9840eab0c5e289781c6bd782021d
MoonShinerYe <maradonaegor@mail.ru>
2023-10-21 06:08:30 +0700

    clown9

--- test.py
+++ test.py
@@ -1,13 +1,54 @@
-from tkinter import *
-from PIL import Image, ImageTk
+import unittest
+from tkinter import Tk
+from unittest.mock import patch
+from io import StringIO
+from main import SurveyApplication
 
-root = Tk()
-root.title("Title")
+class SurveyApplicationTests(unittest.TestCase):
 
-img = Image.open('Paste the directory path')
-bg = ImageTk.PhotoImage(img)
+    def setUp(self):
+        self.app = SurveyApplication()
+        self.app.title("QuizRunner")
+        self.app.geometry("500x500")
 
-lbl = Label(root, image=bg)
-lbl.place(x=0, y=0)
+    def test_init(self):
+        self.assertEqual(self.app.title(), "QuizRunner")
+        self.assertEqual(self.app.geometry(), "500x500")
+        self.assertFalse(self.app.resizable())
 
-mainloop()
+    def test_open_file(self):
+        with patch('tkinter.filedialog.askopenfilename', return_value='sample.txt'):
+            self.app.open_file()
+            self.assertEqual(self.app.filepath, 'sample.txt')
+            self.assertEqual(len(self.app.questions_txt), 2)
+
+    def test_show_title(self):
+        self.app.title_entry.insert(0, "Sample Quiz")
+        self.app.show_title()
+        self.assertEqual(self.app.title_label['text'], "Sample Quiz")
+        self.assertFalse(self.app.title_entry.winfo_ismapped())
+        self.assertFalse(self.app.submit_title_btn.winfo_ismapped())
+
+    def test_add_question(self):
+        self.app.question_entry.insert(0, "What is the capital of France?")
+        self.app.answer_entry.insert(0, "Paris")
+        self.app.add_question()
+        self.assertEqual(len(self.app.questions), 1)
+        question = self.app.questions[0]
+        self.assertEqual(question['question'], "What is the capital of France?")
+        self.assertEqual(question['answer'], "Paris")
+        self.assertEqual(self.app.question_entry.get(), "")
+        self.assertEqual(self.app.answer_entry.get(), "")
+
+    def test_line_switch(self):
+        question = "This is a long question that needs to be wrapped"
+        self.app.question_label.configure(text=question)
+        width_question = self.app.question_label.winfo_width()
+        self.app.line_switch(question)
+        new_width_question = self.app.question_label.winfo_width()
+        self.assertEqual(new_width_question, width_question)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+input()
