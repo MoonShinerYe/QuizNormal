d2210eb4323e0cda7892e5713a0eca799009c3ed
MoonShinerYe <maradonaegor@mail.ru>
2023-11-07 21:38:23 +0700

    Last2

--- test.py
+++ test.py
@@ -1,56 +1,56 @@
 import unittest
-from survey_application import SurveyApplication # Assuming your class is in a file named 'survey_application.py'.
+from survey_application import SurveyApplication  # Assuming your class is in a file named 'survey_application.py'.
 import tkinter as tk
 
 class TestSurveyApplication(unittest.TestCase):
 
-def setUp(self):
-"""Create an instance of the SurveyApplication before each test."""
-self.app = SurveyApplication()
+    def setUp(self):
+        """Create an instance of the SurveyApplication before each test."""
+        self.app = SurveyApplication()
 
-def test_init(self):
-"""Test the __init__ method."""
-self.assertEqual(self.app.title(), "QuizRunner")
-self.assertFalse(self.app.resizable())
-self.assertEqual(len(self.app.questions), 0)
+    def test_init(self):
+        """Test the init method."""
+        self.assertEqual(self.app.title, "QuizRunner")
+        self.assertFalse(self.app.resizable)
+        self.assertEqual(len(self.app.questions), 0)
 
-def test_open_file(self):
-"""Test the open_file method.
+    def test_open_file(self):
+        """Test the open_file method.
 
-This test is tricky because it involves file dialog interaction.
-You'd need to mock the tk.filedialog.askopenfilename method
-to return a predetermined filepath and then test whether the
-questions_txt list is populated correctly.
-"""
-# Mocking would be needed here
-pass
+        This test is tricky because it involves file dialog interaction.
+        You'd need to mock the tk.filedialog.askopenfilename method
+        to return a predetermined filepath and then test whether the
+        questions_txt list is populated correctly.
+        """
+        # Mocking would be needed here
+        pass
 
-def test_load_background(self):
-"""Test if the background is loaded correctly."""
-# You'd need to verify if an image is loaded which is not straightforward
-pass
+    def test_load_background(self):
+        """Test if the background is loaded correctly."""
+        # You'd need to verify if an image is loaded which is not straightforward
+        pass
 
-def test_show_title(self):
-"""Test the show_title method to see if it updates the UI correctly."""
-# This would require simulating button clicks and entry fills
-pass
+    def test_show_title(self):
+        """Test the show_title method to see if it updates the UI correctly."""
+        # This would require simulating button clicks and entry fills
+        pass
 
-def test_create_widgets(self):
-"""Test the create_widgets method."""
-# Check if widgets are created
-pass
+    def test_create_widgets(self):
+        """Test the create_widgets method."""
+        # Check if widgets are created
+        pass
 
-def test_add_question(self):
-"""Test the add_question method."""
-self.app.question_entry.insert(0, "Test question?")
-self.app.answer_entry.insert(0, "Test answer")
-self.app.add_question()
+    def test_add_question(self):
+        """Test the add_question method."""
+        self.app.question_entry.insert(0, "Test question?")
+        self.app.answer_entry.insert(0, "Test answer")
+        self.app.add_question()
 
-self.assertEqual(len(self.app.questions), 1)
-self.assertEqual(self.app.questions[0]['question'], "Test question?")
-self.assertEqual(self.app.questions[0]['answer'], "Test answer")
+        self.assertEqual(len(self.app.questions), 1)
+        self.assertEqual(self.app.questions[0]['question'], "Test question?")
+        self.assertEqual(self.app.questions[0]['answer'], "Test answer")
 
-# Similarly, other methods should be tested.
+    # Similarly, other methods should be tested.
 
 if __name__ == '__main__':
-unittest.main()
+    unittest.main()
